d1634db5e8eff71999582187ac7628c6d2efd8f8
Gourav Dubey <gouravdubey142@gmail.com>
2025-08-13 13:27:50 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+app.use("/", (req, res) => {
+    res.send("Gourav !");
+});
+app.use("/test", (req, res) => {
+    res.send("Testing !");
+});
+app.use("/hello", (req, res) => {
+    res.send("Hello from the server !");
+});
+app.listen(3000, () => {
+    console.log("Server is successfully listening on the port 3000")
+});
