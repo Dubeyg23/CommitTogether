594ef9d67c6b6c4e691fb3f65fece88bf7e69023
Gourav Dubey <gouravdubey142@gmail.com>
2025-08-15 23:28:30 +0530

    dynamic routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => {
-    res.send("Gourav !");
+
+// app.get("/user", (req, res) => {
+//     res.send({ firstName: "Gourav", lastName: "Dubey" });
+// })
+// use of question mark, +, * 
+app.get("/ab?c", (req, res) => {
+    res.send({ firstName: "Gourav", lastName: "Dubey" });
+})
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params)
+    res.send({ firstName: "Gourav", lastName: "Dubey" });
+})
+app.post("/user", (req, res) => {
+    console.log("Save data to the database ");
+    res.send("Data is successfully saved to the database")
 });
+
 app.use("/test", (req, res) => {
     res.send("Testing !");
 });
-app.use("/hello", (req, res) => {
-    res.send("Hello from the server !");
-});
+
 app.listen(3000, () => {
-    console.log("Server is successfully listening on the port 3000")
+    console.log("Server is successfully listening on the port 3000");
 });
