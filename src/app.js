2884f5939b664aaa5ba2979654969a2a80c40076
Gourav Dubey <gouravdubey142@gmail.com>
2025-08-18 19:09:44 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,15 +2,25 @@ const express = require("express");
 
 const app = express();
 
-// app.get("/user", (req, res) => {
-//     res.send({ firstName: "Gourav", lastName: "Dubey" });
-// })
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Gourav", lastName: "Dubey" });
+})
+app.use("/user", (req, res, next) => {
+    // route handler 1
+    console.log("user 1");
+    res.send("Response 1");
+    next()
+}, (req, res) => {
+    // route handler 1
+    console.log("user 2");
+    res.send("Response 2");
+})
 // use of question mark, +, * 
 app.get("/ab?c", (req, res) => {
     res.send({ firstName: "Gourav", lastName: "Dubey" });
 })
 app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params)
+    console.log(req.params);
     res.send({ firstName: "Gourav", lastName: "Dubey" });
 })
 app.post("/user", (req, res) => {
