5520f1311b20688a77afa19d63635656f2dcd724
Gourav Dubey <gouravdubey142@gmail.com>
2025-08-19 18:56:41 +0530

    created a post api to add user to the database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
-const connectDb = require("./config/database")
-const app = express()
+const connectDb = require("./config/database");
+const User = require("./models/User");
+const app = express();
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "Virat",
+        lastName: "Kohli",
+        emailId: "virat@gmai;.com",
+        password: "virat@123"
+    }
+    // Creating a new instance of the user model
+    const user = new User(userObj)
+    try {
+        await user.save();
+        res.send("User Added Successfully!")
+    } catch (err) {
+        res.status(400).send("Error saving the user:" + err.message);
+    }
+})
 connectDb().then(() => {
     console.log("Database connection established");
     app.listen(3000, () => {
